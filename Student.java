61e881852a451c3df11a1d165a53b2f681c9b894
TH2oo <76653651+TH2oo@users.noreply.github.com>
2025-02-12 15:57:20 +0000

    Ajout de la classe student.java

---
+++ Student.java
@@ -0,0 +1,35 @@
+import java.util.List;
+import java.util.ArrayList;
+
+public class Student {
+    private final String firstName;
+    private final String lastName;
+    private final List<TeachingUnitResult> results;
+    
+    public Student(String firstName, String lastName){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.results = results;
+    }
+
+    public boolean equals(Object o){
+        return this == o;
+    }
+
+    public void addResult(String teachingUnitName, Grade grade){
+        this.results.add(new TeachingUnitResult(teachingUnitName, grade));
+    }
+
+    public List<Grade> getGrades(){
+        List<Grade> grades = new ArrayList<>();
+
+        for (TeachingUnitResult result : results){
+            grades.add(result.getGrade());
+        }
+        return grades;
+    }
+
+
+    
+
+}
