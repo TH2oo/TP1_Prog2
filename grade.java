36677efcd50313cd6a76b8b8f38cd3fc277a9a26
TH2oo <76653651+TH2oo@users.noreply.github.com>
2025-02-12 14:40:32 +0000

    Création de la classe grade

---
+++ grade.java
@@ -0,0 +1,29 @@
+public class Grade {
+    private static final int MAXIMUM_GRADE = 20;
+    private final double value;
+
+    public Grade(double value){
+        this.value = value;
+    }
+
+    public boolean equals(Object o){
+        return this == o;
+    }
+
+    public double getValue(){
+        return this.value;
+    }
+
+    public string toString(){
+        return this.value + "/" + MAXIMUM_GRADE;
+    }
+    
+    public static Grade averageGrade(List<Grade> grades){
+        double sumOfGrades = 0;
+        for (int i = 0; i < grades.size(); i++){
+            sumOfGrades += grades.get(i).getValue();
+        }
+        return new Grade(sumOfGrades/grades.size());
+    }
+
+}
