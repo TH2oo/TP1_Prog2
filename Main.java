c7f01dbfcdfed179d1b4d56d22163bb53087007e
Théo Saez <dkgtheo44@gmail.com>
2025-02-13 16:08:58 +0100

    Test de si un étudiant à une note ABS

--- Main.java
+++ Main.java
@@ -4,6 +4,7 @@ public class Main {
         Student student1 = new Student("Théo", "Saez");
         Student student2 = new Student("Edouard", "Dansette");
         Student student3 = new Student("Annibal", "Pété");
+        Student student4 = new Student("Le", "Lobotomisateur");
 
         // Ajouter des notes aux étudiants
         student1.addResult("Programmation 2", new Grade(18.0));
@@ -15,6 +16,9 @@ public class Main {
         student3.addResult("Programmation 2", new Grade(8.0));
         student3.addResult("Structures discrètes", new Grade(3.0));
 
+        student4.addResult("Programmation 2", new Grade());
+        student4.addResult("Structures discrètes", new Grade(12.0));
+
         // Créer une promotion (instance de Cohort)
         Cohort cohort = new Cohort("L2 informatique");
 
@@ -22,6 +26,7 @@ public class Main {
         cohort.addStudent(student1);
         cohort.addStudent(student2);
         cohort.addStudent(student3);
+        cohort.addStudent(student4);
 
         // Afficher les résultats de la promotion
         cohort.printStudentsResults();
