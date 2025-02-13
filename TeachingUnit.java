f0234baea05bed291333addfe9f6efdfac42016d
Théo Saez <dkgtheo44@gmail.com>
2025-02-13 18:33:43 +0100

    Ajout de la classe TeachingUnit pour représenter une unité d'enseignement avec un nom et un token

---
+++ TeachingUnit.java
@@ -0,0 +1,17 @@
+public class TeachingUnit {
+    private final String name;
+    private final int token;
+
+    public TeachingUnit(String name, int token){
+        this.name = name;
+        this.token = token;
+    }
+
+    public String toString(){
+        return "L'UE " + this.name + " possède " + this.token;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+}
